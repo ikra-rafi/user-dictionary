e6cd9a48810b612f597fc62a7bb7581b7832a781
Ikra Rafi <irafi@nlihc.org>
2021-03-31 22:12:43 -0400

    deleting extra files created by react npx

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
     getUsers: function () {
-        // Return Data from Randomuser API 
+
         return axios.get("https://randomuser.me/api/?results=200&nat=us");
     }
 };
